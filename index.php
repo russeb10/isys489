d236c114b2967a664da16016f689e410a15bf87a
gerndtp <gerndtp@ferris.edu>
2015-01-18 20:48:31 -0500

    Index.html
    Original test file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php # Script 3.4 - index.php
+$page_title = 'Welcome to this ISYS489 GR!';
+print 'Hello World';
+?>
+<h1 id="mainhead">Big Header</h1>
+<p>This is where you'll put the main page content. This content will differ for each page.</p>
+<p>This is where you'll put the main page content. This content will differ for each page.</p>
+<p>This is where you'll put the main page content. This content will differ for each page.</p>
+<p>This is where you'll put the main page content. This content will differ for each page.</p>
+<h2>Subheader</h2>
+<p>This is where you'll put the main page content. This content will differ for each page.</p>
+<p>This is where you'll put the main page content. This content will differ for each page.</p>
+<p>This is where you'll put the main page content. This content will differ for each page.</p>
+<p>This is where you'll put the main page content. This content will differ for each page.</p>
+<?php
+Print 'Goodbye World';
+?>
